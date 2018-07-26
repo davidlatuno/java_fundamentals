1780ca1e0121330e1faa9cb59a32bbf137bec2a7
davidlatuno <dslatuno@gmail.com>
2018-07-26 16:48:28 -0700

    Added more list functionality: append

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -49,6 +49,40 @@ class LinkedList {
         prev_node.next = new_node;
     }
 
+    public void append (int new_data) {
+        /* 1. Allocate the Node &
+            2. Put in the data
+         */
+
+        Node new_node = new Node(new_data);
+
+        /* 3. If the Linked List is empty, then make the new node as head
+         */
+
+        if (head == null) {
+            head = new Node(new_data);
+            return;
+        }
+
+        /* 4. This new node is going to be the last node, so make next of it as null
+
+         */
+
+        new_node.next = null;
+
+        /* 5. Else traverse till the last node */
+
+        Node last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+
+        /* 6. Change the next of last node */
+        last.next = new_node;
+        return;
+    }
+
+
     public static void main(String[] args) {
         // write your code here
         LinkedList list= new LinkedList();
@@ -61,10 +95,17 @@ class LinkedList {
 
         second.next = third;
 
+        System.out.println("Original Linked List");
         list.printList();
-
+        System.out.println("Adding new node to front");
         list.push(1);
-
         list.printList();
+        System.out.println("Adding new node to specific spot");
+        list.insertAfter(list.head, 9);
+        list.printList();
+        System.out.println("Appending node");
+        list.append(13);
+        list.printList();
+
     }
 }
